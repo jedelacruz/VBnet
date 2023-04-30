bf2c4f8fe9e87d938caf3a2e1b5b64a513227f6f
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-04-30 23:43:39 +0800

    Create variables.vb

---
+++ variables.vb
@@ -0,0 +1,13 @@
+Public Class Form1
+  
+      Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+
+        Dim firstName As String
+
+        firstName = "Je"
+
+        MsgBox("hello and welcome " & firstName)
+
+    End Sub
+
+End Class
