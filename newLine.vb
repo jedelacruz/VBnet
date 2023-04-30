c596e1cb34da1d52737f85365ba99f55bab6cd03
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-04-30 23:54:53 +0800

    Create newLine.vb

---
+++ newLine.vb
@@ -0,0 +1,15 @@
+Public Class Form1
+  
+  Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+
+        Dim firstName As String
+        Dim lastName As String
+
+        firstName = "Je"
+        lastName = "Dela Cruz" 
+
+        MsgBox(firstName & vbNewLine & lastName)
+
+    End Sub
+
+End Class
