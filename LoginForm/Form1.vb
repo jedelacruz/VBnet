da2ace419b83b155b381b62bc6f6817befa729df
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-05-03 17:10:37 +0800

    Create Form1.vb

---
+++ LoginForm/Form1.vb
@@ -0,0 +1,35 @@
+Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim tvlDepUsername As String
+        Dim tvlDepPassword As String
+
+        tvlDepUsername = "tvluser"
+        tvlDepPassword = "12345"
+
+        Dim acadsDepUsername As String
+        Dim acadsDepPassword As String
+
+        acadsDepUsername = "acaduser"
+        acadsDepPassword = "54321"
+
+
+        Dim Username As String
+        Dim Password As String
+
+        Username = TextBox1.Text
+        Password = TextBox2.Text
+
+        If Username = tvlDepUsername AndAlso Password = tvlDepPassword Or Username = acadsDepUsername AndAlso Password = acadsDepPassword Then
+            Form2.Show()
+            Me.Hide()
+        Else
+            MsgBox("Invalid Account")
+        End If
+
+    End Sub
+End Class
